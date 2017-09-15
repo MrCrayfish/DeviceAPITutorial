1aa4e38c843d226bf37f4407d22617f6c1645f2f
MrCrayfish <mrcrayfish@hotmail.com>
2017-09-15 14:24:42 +0930

    Initial setup

---
+++ src/main/java/com/mrcrayfish/tutorial/Reference.java
@@ -0,0 +1,12 @@
+package com.mrcrayfish.tutorial;
+
+/**
+ * Author: MrCrayfish
+ */
+public class Reference
+{
+    public static final String MOD_ID = "app_tutorial";
+    public static final String MOD_NAME = "Tutorial App";
+    public static final String MOD_VERSION = "0.0.1";
+    public static final String MC_VERSION = "[1.11,1.11.2]";
+}
---
+++ src/main/java/com/mrcrayfish/tutorial/TutorialMod.java
@@ -0,0 +1,18 @@
+package com.mrcrayfish.tutorial;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+/**
+ * Author: MrCrayfish
+ */
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION, acceptedMinecraftVersions = Reference.MC_VERSION)
+public class TutorialMod
+{
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+}
